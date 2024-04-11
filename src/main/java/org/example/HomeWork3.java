bcb98108cdc5f81db9d6fd408b42f21be975a7f6
VladOstapchuk <vlad.ostapchuk@gmail.com>
2024-04-11 15:09:59 +0300

    HomeWork3 ready

--- src/main/java/org/example/HomeWork3.java
+++ src/main/java/org/example/HomeWork3.java
@@ -9,25 +9,25 @@ public class HomeWork3 {
 
     public static void main(String[] args) {
 
-        int arraylength;
-        boolean isChanged = true;
-        int transfer;
+        int arraylength;                         //array length variable
+        boolean isChanged = true;                //monitor for sorting of array
+        int transfer;                            //to replace the values of adjacent array cells
 
         System.out.println("Please enter Array length and press Enter: ");
-        Scanner console = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);  // entering array length from console
         arraylength = console.nextInt();
 
         int[] array = new int[arraylength];
         System.out.println("Array before sorting: ");
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100);
+            array[i] = (int) (Math.random() * 100);     //fill the array with random integers from 0 to 100
             System.out.print(array[i] + " ");
         }
 
 
-        while (isChanged == true) {
+        while (isChanged == true) {                                  //sorting array
             isChanged = false;
-            for (int i = 0; i < array.length - 1; i++) {
+            for (int i = 0; i < array.length - 1; i++) {             //we sort the array by comparing adjacent values
                 if (array[i] > array[i + 1]) {
                     transfer = array[i + 1];
                     array[i + 1] = array[i];
@@ -39,7 +39,7 @@ public class HomeWork3 {
 
         }
         System.out.println();
-        System.out.println("Array after sorting: ");
+        System.out.println("Array after sorting: ");            //output to the console for sorted array
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
