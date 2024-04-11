c3d88fe203207d0841c778566299ca35bac1e13e
VladOstapchuk <vlad.ostapchuk@gmail.com>
2024-04-11 15:27:27 +0300

    HomeWork3 - 2 variants for Sorting -  from smallest to largest and sort from largest to smallest.

--- src/main/java/org/example/HomeWork3.java
+++ src/main/java/org/example/HomeWork3.java
@@ -12,6 +12,8 @@ public class HomeWork3 {
         int arraylength;                         //array length variable
         boolean isChanged = true;                //monitor for sorting of array
         int transfer;                            //to replace the values of adjacent array cells
+        int sort;                                //how to sort array
+
 
         System.out.println("Please enter Array length and press Enter: ");
         Scanner console = new Scanner(System.in);  // entering array length from console
@@ -24,20 +26,44 @@ public class HomeWork3 {
             System.out.print(array[i] + " ");
         }
 
+        System.out.println();
+        System.out.println("To sort the array from smallest to largest - press 1.\n" +    //How to sort array?
+                "To sort the array from largest to smallest - press 2 :");
+        sort = console.nextInt();
 
-        while (isChanged == true) {                                  //sorting array
-            isChanged = false;
-            for (int i = 0; i < array.length - 1; i++) {             //we sort the array by comparing adjacent values
-                if (array[i] > array[i + 1]) {
-                    transfer = array[i + 1];
-                    array[i + 1] = array[i];
-                    array[i] = transfer;
-                    isChanged = true;
+        if (sort == 1){
+            while (isChanged == true) {                                  //sorting array
+                isChanged = false;
+                for (int i = 0; i < array.length - 1; i++) {             //we sort the array by comparing adjacent values
+                    if (array[i] > array[i + 1]) {
+                        transfer = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = transfer;
+                        isChanged = true;
+                    }
                 }
             }
-
-
         }
+
+        if (sort == 2){
+            while (isChanged == true) {                                  //sorting array
+                isChanged = false;
+                for (int i = 0; i < array.length - 1; i++) {             //we sort the array by comparing adjacent values
+                    if (array[i] < array[i + 1]) {
+                        transfer = array[i];
+                        array[i] = array[i+1];
+                        array[i+1] = transfer;
+                        isChanged = true;
+                    }
+                }
+            }
+        }
+        else
+            System.out.println("Wrong choice, try again");
+
+
+
+
         System.out.println();
         System.out.println("Array after sorting: ");            //output to the console for sorted array
         for (int i = 0; i < array.length; i++) {
